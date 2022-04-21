d7763c3ba22152905ff67b893a77e9d68a15704f
lucianoagustin.ditzelramirez.contractor.gdevtools.com <lucianoagustin.ditzelramirez.contractor@gdevtools.com>
2022-04-20 23:00:49 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ function mostrarScroll() {
   let scrollTop = document.documentElement.scrollTop;
   for (var i = 0; i < animado.length; i++) {
     let alturaAnimado = animado[i].offsetTop;
-    if (alturaAnimado - 520 < scrollTop) {
+    if (alturaAnimado - 600 < scrollTop) {
       animado[i].style.opacity = 1;
       animado[i].classList.add("mostrarArriba");
     }
